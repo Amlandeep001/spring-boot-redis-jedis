3901b846c42b0efd2440011d21f8e816726837d1
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2024-05-22 02:41:40 +0530

    Updating config properties file
    Updating config properties file

--- src/main/java/com/org/springboot/redis/config/properties/RedisProperties.java
+++ src/main/java/com/org/springboot/redis/config/properties/RedisProperties.java
@@ -1,16 +1,9 @@
 package com.org.springboot.redis.config.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.experimental.FieldDefaults;
-
-@Data
-@FieldDefaults(level = AccessLevel.PRIVATE)
-@Configuration
 @ConfigurationProperties(prefix = "redis.server")
+@lombok.Value
 public class RedisProperties
 {
 	String hostname;
