f66eb9dc2f71aeae0ca971aac01e347db96b92dd
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2025-01-28 02:06:04 +0530

    Fixed the problems in config file

--- src/main/java/com/org/springboot/redis/config/properties/RedisProperties.java
+++ src/main/java/com/org/springboot/redis/config/properties/RedisProperties.java
@@ -1,9 +1,16 @@
 package com.org.springboot.redis.config.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Configuration
 @ConfigurationProperties(prefix = "redis.server")
-@lombok.Value
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class RedisProperties
 {
 	String hostname;
